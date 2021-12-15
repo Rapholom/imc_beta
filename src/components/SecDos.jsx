d139935aeb613122f414b9e67e9845ed16afcb76
Edgar Alexander Haro Garcia <harove@gmail.com>
2021-12-15 00:17:02 -0300

    colaborando

--- src/components/SecDos.jsx
+++ src/components/SecDos.jsx
@@ -22,7 +22,7 @@ const SecDos = () => {
 
                 <div className="col m6 s12">
                     <div className="secDosRight z-depth-2">
-
+                            Hola
                     </div>
                 </div>
 
